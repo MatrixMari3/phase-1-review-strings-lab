c87fbc421a68b74308c2349d1c8f2b339081e4e9
Smaqiola <marieacholaa@gmail.com>
2024-03-21 22:12:58 +0300

    Complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const upperCasecurrentUser = 'GRACE HOPPER'
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCasecurrentUser}!`;
+const firstInitial = 'currentUser {G}'
+const shortGreeting = `Welcome, ${firstInitial}!`;
